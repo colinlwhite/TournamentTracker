61321f84d7bdad606e7358df2b8a89126d144e6f
Colin White <clwhite814@gmail.com>
2020-09-10 13:55:39 -0500

    WIP: Cleaning up folder structure

--- TrackerLibrary/Models/TournamentModel.cs
+++ TrackerLibrary/Models/TournamentModel.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace TrackerLibrary
+namespace TrackerLibrary.Models
 {
     /// <summary>
     /// Represents one tournament, with all of the rounds, matchups, prizes and outcomes. 
