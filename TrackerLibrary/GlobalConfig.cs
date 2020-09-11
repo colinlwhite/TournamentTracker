7250fe6192a08c3eb008840be66b3c3e7bf305d9
Colin White <clwhite814@gmail.com>
2020-09-11 10:37:50 -0500

    WIP

--- TrackerLibrary/GlobalConfig.cs
+++ TrackerLibrary/GlobalConfig.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -36,6 +37,13 @@ namespace TrackerLibrary
                 TextConnector text = new TextConnector();
                 Connections.Add(text);
             }
+        }
+
+        public static string CnnString(string name)
+        {
+            // Going to App.Config to get me the connection string by looking
+            // up the name we gave it. It's going to return the ConnectionString attribute vALUE
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
         }
     }
 }
