c5915aec887d1bb4810e4362372d3338ca852157
Colin White <clwhite814@gmail.com>
2020-09-14 08:51:29 -0500

    updated the connection string server to localhost and we're getting data back the correct data in SSMS

--- TrackerLibrary/DataAccess/SqlConnector.cs
+++ TrackerLibrary/DataAccess/SqlConnector.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Dapper;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,9 +19,28 @@ namespace TrackerLibrary.DataAccess
         /// <returns>The prize information, including the unique indentifier.</returns>
         public PrizeModel CreatePrize(PrizeModel model)
         {
-            model.Id = 1;
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(GlobalConfig.CnnString("Tournaments")))
+            {
+                // DynamicParameters a method from Dapper
+                var p = new DynamicParameters();
+                // The parameter inside SSMS and value we're passing in
+                p.Add("@PlaceNumber", model.PlaceNumber);
+                p.Add("@PlaceName", model.PlaceName);
+                p.Add("@PrizeAmount", model.PrizeAmount);
+                p.Add("@PrizePercentage", model.PrizePercentage);
+                // This is unique because it's coming out, rather than being sent in, Therefore:
+                // So we're going to pass a value in of 0 and say dbType is an Int32
+                // ParameterDirection is an enum, so it's enumerating the options we have available to us
+                // By default, it's an input that why this isn't specified in the aforementioned parameters
+                p.Add("@id", 0, dbType: DbType.Int32, direction: ParameterDirection.Output);
 
-            return model;
+                // Running the stored procedure
+                connection.Execute("dbo.spPrizes_Insert", p, commandType: CommandType.StoredProcedure);
+                // <T> Generic example because we've specified the type as "int"
+                model.Id = p.Get<int>("@id");
+
+                return model;
+            }
         }
     }
 }
