7a9714599bfe21c5ab397f609e717d35cb5d45b1
Colin White <clwhite814@gmail.com>
2020-09-21 16:28:11 -0500

    wip

--- TrackerLibrary/DataAccess/TextConnector.cs
+++ TrackerLibrary/DataAccess/TextConnector.cs
@@ -13,10 +13,13 @@ namespace TrackerLibrary
         // TODO - Wire up the CreatePrize for text files. 
         public PrizeModel CreatePrize(PrizeModel model)
         {
-
-            model.Id = 1;
-
-            return model;
+            // Load the text file - (not based on a certain model)
+            // Convert the text to List<PrizeModel>
+            // Find the max ID using LINQ
+            // Add the new record with the new ID (max + 1)
+            // Convert the prizes to List<string>
+            // Save the List<string> to the text file
+            
         }
     }
 }
---
+++ TrackerLibrary/DataAccess/TextConnectorProcessor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary.DataAccess.TextConnector
+{
+    public static class TextConnectorProcessor
+    {
+        public static string FullFilePath(string fileName)
+        {
+            // fileName example: PrizeModels.csv
+
+            return $"";
+        }
+    }
+}
