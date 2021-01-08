703fdf216c307ac4eece185e464ec219f9c3b77b
Colin White <clwhite814@gmail.com>
2021-01-08 09:31:09 -0600

    We're displaying the teams and team members list

---
+++ MVCUI/Controllers/TeamsController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TrackerLibrary;
+using TrackerLibrary.Models;
+
+namespace MVCUI.Controllers
+{
+    public class TeamsController : Controller
+    {
+        // GET: Teams
+        public ActionResult Index()
+        {
+            List<TeamModel> allTeams = GlobalConfig.Connection.GetTeam_All();
+            return View(allTeams);
+        }
+
+        // GET: Teams/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Teams/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+    }
+}
