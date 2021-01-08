2ac01f0eac60f9bd2bb655a2e08751bc203de084
Colin White <clwhite814@gmail.com>
2021-01-08 14:07:10 -0600

    Finished the create team page

--- MVCUI/Controllers/TeamsController.cs
+++ MVCUI/Controllers/TeamsController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVCUI.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -20,18 +21,39 @@ namespace MVCUI.Controllers
         // GET: Teams/Create
         public ActionResult Create()
         {
-            return View();
+            // Get the people
+            List<PersonModel> availablePlayers = GlobalConfig.Connection.GetPerson_All();
+
+            // Create a TeamMVCModel
+            TeamMVCModel model = new TeamMVCModel();
+
+            // Populate TeamMVCModel.TeamMembers via LINQ because it's a SelectListItem type
+            model.TeamMembers = availablePlayers.Select(x => new SelectListItem { Text = x.FullName, Value = x.Id.ToString() }).ToList();
+
+            return View(model);
         }
 
         // POST: Teams/Create
+        [ValidateAntiForgeryToken()]
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Create(TeamMVCModel model)
         {
             try
             {
-                // TODO: Add insert logic here
+                if (ModelState.IsValid && model.SelectedTeamMembers.Count > 0)
+                {
+                    TeamModel t = new TeamModel();
+                    t.TeamName = model.TeamName;
+                    t.TeamMembers = model.SelectedTeamMembers.Select(x => new PersonModel { Id = int.Parse(x) }).ToList();
 
-                return RedirectToAction("Index");
+                    GlobalConfig.Connection.CreateTeam(t);
+
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return RedirectToAction("Create");
+                }
             }
             catch
             {
