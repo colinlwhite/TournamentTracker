8971d4d064fd8186a8b4f4f0d4a217ffa1fbf169
Colin White <clwhite814@gmail.com>
2021-01-10 07:59:46 -0600

    WIP: Starting to work on 3 columns for the Create Touornament Page

---
+++ MVCUI/Controllers/TournamentsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TrackerLibrary;
+using TrackerLibrary.Models;
+
+namespace MVCUI.Controllers
+{
+    public class TournamentsController : Controller
+    {
+        // GET: Tournaments
+        public ActionResult Index()
+        {
+            return RedirectToAction("Index","Home");
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: People/Create
+        [HttpPost]
+        public ActionResult Create(TournamentModel p)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    // TODO - Create the Tournament
+
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
